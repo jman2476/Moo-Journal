ee8aa72d657fac4dac545e10f8b42f44749381a0
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-24 11:58:54 -0800

    testing hp routes

--- client/src/components/adminControl.jsx
+++ client/src/components/adminControl.jsx
@@ -1,9 +1,10 @@
 function AdminControl({adminObj : {runFxn, msg}}) {
     // console.log(props)
     return (
-        <div className='adminControl flex flex-row items-center'>
-            <p>ADMIN</p>
+        <div className='adminControl flex flex-row items-center bg-green pa1'>
+            <p className="mj-text fw8">ADMIN</p>
             <button onClick={runFxn} className="admin-btn">{msg}</button>
+            {/* <button>Hide</button> */}
         </div>
     )
 }
