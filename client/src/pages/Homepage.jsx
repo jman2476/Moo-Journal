9c81f06a69b577716e4e05f208e7274b7516c23a
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-24 11:05:59 -0800

    add backdrop, basic stylings for hp

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import cowPic from '../assets/cow-hp-bg.jpg';
+import { useState } from 'react'
+function Homepage() {
+
+    const [isLoggedIn, setLoggedIn] = useState(true)
+    const userName = 'cjswayne'
+
+    return (
+        <>
+            <div className="hp-backdrop">
+                <img src={cowPic} alt="Background" className="hp-image" />
+                <img src={cowPic} alt="Background" className="hp-image" />
+            </div>
+            <div className='homepage' style={{ position: 'relative', zIndex: 1 }}>
+                <h1  className="modak-regular">MooJournal</h1>
+                { !isLoggedIn ? 
+                    <>
+                        <h3  className="modak-regular">Start your MooJournaling journey.</h3>
+                        <span>
+                            <button>Login</button>
+                            <button>Sign Up</button>
+                        </span> 
+                    </>
+                    :
+                    <>
+                        <h3  className="modak-regular">Welcome Back, {userName}</h3>
+                        <span>
+                            <button>Start Writing</button>
+                        </span> 
+                    </>
+                    
+                }
+                
+                
+            </div>
+            <style>{`
+                @keyframes slide {
+                    from { transform: translateX(0); }
+                    to { transform: translateX(-50%); } // Moves the container to show the second image, then resets
+                }
+                body{
+                    overflow:hidden;
+                }
+            `}</style>
+        </>
+    );
+}
+
+export default Homepage;
