80b5efb2e39a1e05e21daefe2bb1c3682873d37b
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-24 11:54:22 -0800

    changed font styles

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import cowPic from '../assets/cow-hp-bg.jpg';
 import { useState } from 'react'
 import AdminControl from '../components/adminControl';
+import { getUserName } from '../../services/userService';
 
 
 function Homepage() {
-
-
-    // TODO: finish this function so that it will return the username of the user
-    // logged in
-    const getUserName = () => {
-        return 'cjswayne'
-    }
 
     const [isLoggedIn, setLoggedIn] = useState(false)
     const userName = getUserName()
@@ -28,8 +22,6 @@ function Homepage() {
         msg:'Change Logged in state'
     }
 
-
-
     return (
         <>
             <AdminControl adminObj={adminObj} />
@@ -41,25 +33,23 @@ function Homepage() {
             <div className='homepage' style={{ position: 'relative', zIndex: 1 }}>
                 <h1  className="modak-regular">MooJournal</h1>
                 { isLoggedIn ? 
-
                     <>
-                        <h3  className="modak-regular">Welcome back, {userName}</h3>
+                        <h3>Welcome back, <span className='mj-text'>{userName}</span></h3>
                         <span>
                             <button>Start Writing</button>
+                            <button className='mj-text fw8'>My MooJournal</button>
                         </span> 
                     </>
                     :
                     <>
-                        <h3  className="modak-regular">Start your MooJournaling journey.</h3>
+                        <h3>Start your <span className=''> MooJournaling </span> journey.</h3>
                         <span>
+                            <button>What the hell is <span className='mj-text fw8'>MooJournal</span>?</button>
                             <button>Login</button>
                             <button>Sign Up</button>
                         </span> 
-                    </>
-                    
-                }
-                
-                
+                    </>   
+                }           
             </div>
             <style>{`
                 @keyframes slide {
