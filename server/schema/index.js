0c3fcaae0a2ec0e6e234fea497ca532f207b3132
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-02-29 14:18:42 -0500

    added empty resolvers and typeDefs, and exported them to index.js so that the server doesnt try to kill itself

---
+++ server/schema/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    resolvers: require('./resolvers'),
+    typeDefs: require('./typeDefs')
+}
---
+++ server/schema/typeDefs.js
@@ -0,0 +1,7 @@
+const gql = String.raw
+
+// typeDefs explain the resolver
+const typeDefs = gql``
+
+
+module.exports = typeDefs
