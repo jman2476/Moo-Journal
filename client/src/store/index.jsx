e91705497e1d32f235580c8b5ef51d63265f2188
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-29 16:06:45 -0800

    store provider added to main.jsx

--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -1,17 +1,28 @@
 import { useContext, createContext, useState, useEffect } from "react"
 import { useQuery } from '@apollo/client'
-import {AUTHENTICATE} from '../graphql'
+// import {AUTHENTICATE} from '../graphql'
 
 const Context = createContext()
 
 export function StoreProvider({children}){
 
-    // const { data: userData } = useQuery(AUTHENTICATE)
-
+    // const { data: userData } = useQuery(AUTHENTICATE) // correct userData obj
+    const userData = { authenticate: true } // simulated obj
 
     const [state, setState] = useState({
-        user:null
+        user:null,
+        loading: true
     })
+
+    useEffect(() => {
+        if(userData){
+            setState({
+                ...state,
+                user:userData.authenticate,
+                loading:false
+            })
+        }
+    }, [userData])
 
 
     return (
