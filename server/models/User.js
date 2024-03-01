13836c47415c3a9378ddee72f107226fa698d85b
shannontice <stice14@gmail.com>
2024-03-01 11:17:42 -0500

    models started

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const { model, Schema } = require('mongoose');
+const { hash, compare } = require('bcrypt')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: [true, 'Please enter a valid username'],
+            minLength: [5, 'Your username must be at least 5 characters long.']
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'Please enter a valid email address'],
+            validate: {
+                validator(val) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ig.test(val);
+                },
+                message: 'Your email address is invalid.'
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: [5, 'Your password must be at least 5 characters long.']
+        },
+        journal: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Journal'
+        }]
+
+    }
+)
+
+userSchema.methods,validatePass = async function (formPass) {
+    const validPass = await compare(formPass, this.password);
+
+    return validPass
+}
+
+userSchema.pre('save', async function (next) {
+    if(this.isNew){
+        this.password= await hash(this.password, 10);
+    }
+    next();
+});
+
+const User = model('User', userSchema)
+model.exports = User;
