44fdef2f42acca75abe81e7bc6256888f2f997b1
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-03-01 18:56:26 -0500

    ready for deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/moo_journal_db')
+mongoose.connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/moo_journal_db')
 
 module.exports = mongoose.connection
