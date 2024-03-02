7d98b81de5caf937a5f1dac30976c256dd363ca7
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-03-01 20:26:33 -0800

    finished basic homepage header

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -36,10 +36,12 @@ function Homepage() {
         <>
             <header className={headerClassName}>
             
-                <h1 className="modak-regular"><a href="/">MooJournal</a></h1>
                 {state.user ?
                     <>
+                    <span className="logoContainer">
+                        <h1 className="modak-regular"><a href="/">MooJournal</a></h1>
                         <h3>Welcome back, <span className='mj-text'>{state.user.username}</span></h3>
+                        </span>
                         <span>
                             <AddNewEntry />
                             <NavLink className="btn" to='/my_journal'>
@@ -52,7 +54,10 @@ function Homepage() {
                     </>
                     :
                     <>
-                        <h3>Start your <span className=''> MooJournaling </span> journey.</h3>
+                    <span className="logoContainer">
+                        <h1 className="modak-regular"><a href="/">MooJournal</a></h1>
+
+                        <h3>Start your <span className=''> MooJournaling </span> journey.</h3></span>
                         <span>
                             <NavLink className="btn" to='/about'>What the hell is <span className='mj-text fw8'>MooJournal</span>?</NavLink>
                             <NavLink className="btn" to='/auth'>Login | Sign Up</NavLink>
