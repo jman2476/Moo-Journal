01ee973235be797094baf919614b23e8794e3422
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-03-02 18:53:34 -0800

    finished BASIC myMoojournal page

--- client/src/pages/MyMooJournal.jsx
+++ client/src/pages/MyMooJournal.jsx
@@ -1,17 +1,27 @@
 // This will be the equivalent of the user's dashboard
-import {GET_USER_NOTES} from '../graphql/mutations'
-import { useQuery, useMutation } from '@apollo/client'
+import { GET_USER_NOTES } from '../graphql/queries'
+import { useQuery } from '@apollo/client'
 
 
 
-function MyMooJournal(){
+function MyMooJournal() {
 
-    const data = useQuery(GET_USER_NOTES)
+    const { data: entryData } = useQuery(GET_USER_NOTES)
+    // console.log(data)
 
     return (
         <>
             <h1>MyMooJournal</h1>
-            <button onClick={() => console.log('notes')}>Get notes</button>
+            <button onClick={() => console.log(entryData)}>Get notes</button>
+            <div className="entry-container overflow-auto">
+            {!entryData?.getUserEntries.length && <h2>You have not created any Entries.</h2>}
+                {entryData?.getUserEntries.map((entry, index) => (
+                    <div key={entry._id}>
+                        <h3>{entry.text}</h3>
+                    </div>
+                ))}
+            </div>
+            
         </>
     )
 }
