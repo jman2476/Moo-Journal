e953d2caf84b26161dabe2989cb91eb291c595f9
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-03-04 10:22:51 -0800

    getting errors

--- client/src/pages/MyMooJournal.jsx
+++ client/src/pages/MyMooJournal.jsx
@@ -26,22 +26,39 @@ function MyMooJournal() {
 
 
     const togglePrompt = (entry) => {
-        return (
-            <span>
-                <p className={showPrompt[entry._id] ? "ma0 pa0" : "truncate ma0 pa0"}>{entry.prompt.text}</p>
-                <p className="f7 ma0 pa0 pointer hover-white" onClick={() => togglePromptVis(entry._id)}>{showPrompt[entry._id] ? "Hide Prompt" : "Show Prompt"}</p>
-            </span>
-        )
-    }
-
-    const toggleEntry = (entry) => {
+        // Determine the text to display based on the showPrompt state
+        const displayText = showPrompt[entry._id] ? entry.prompt.text : truncateText(entry.prompt.text, 105); // Adjust 100 to your desired length
+    
         return (
             <>
-                <p className={showEntry[entry._id] ? "ma0 pa0" : "truncate ma0 pa0" }>{entry.text}</p>
-                <p className="f7 ma0 pa0 pointer hover-white" onClick={() => toggleEntryVis(entry._id)}>{showEntry[entry._id] ? "Hide Entry" : "Show Full Entry" }</p>
+                <p className="ma0 pa0">{displayText}</p>
+                <p className="f7 ma0 pa0 mb2 pointer hover-white" onClick={() => togglePromptVis(entry._id)}>
+                    {showPrompt[entry._id] ? "Hide Prompt" : "Show Prompt"}
+                </p>
             </>
-        )
+        );
+    };
+    
+    const toggleEntry = (entry) => {
+        // Similar approach for entry text
+        const displayText = showEntry[entry._id] ? entry.text : truncateText(entry.text, 105); // Adjust 100 to your desired length
+    
+        return (
+            <>
+                <p className="ma0 pa0">{displayText}</p>
+                <p className="f7 ma0 pa0 pointer hover-white" onClick={() => toggleEntryVis(entry._id)}>
+                    {showEntry[entry._id] ? "Hide Entry" : "Show Full Entry"}
+                </p>
+            </>
+        );
+    };
+    
+
+    function truncateText(text, maxLength) {
+        if (text.length <= maxLength) return text;
+        return text.substr(0, maxLength) + '...';
     }
+    
 
 
     const togglePromptVis = (id) => {
@@ -76,6 +93,7 @@ function MyMooJournal() {
             {/* <h1>MyMooJournal</h1> */}
             {/* <button onClick={() => console.log(entryData)}>Get notes</button> */}
             <div className="entry-container overflow-auto flex flex-column items-start ">
+            <h2 className="fw1 mr2">My Journal Entries</h2>
                 {!entryData?.getUserEntries.length && <h2>You have not created any Entries.</h2>}
                 {entryData?.getUserEntries.map((entry, index) => (
                     <div style={{
@@ -83,20 +101,29 @@ function MyMooJournal() {
                         borderStyle: 'solid', // Ensure the border is visible by setting a style
                         borderWidth: '3px' // Set a default border width
                     }} key={entry._id} className="ba br4 ma2 journalEntryCard tl">
-                        <h4 className="w-100 flex justify-between ma2 mt3 mj-text">{dayjs(entry.createdAt).format('MM/DD/YYYY [at] hh:mm a')}<span className="pa1 ph2 ml1 br3 mb1 mr2" style={{ backgroundColor: moods[entry.moodRanking].color, color: +entry.moodRanking === 5 || +entry.moodRanking === 4 ? 'black' : 'white' }}>Mood: {moods[entry.moodRanking].mood}</span></h4>
-                        <div className="ma2">
+ 
+
+
+
+                        <h4 className="w-100 flex justify-between ma2 mt3 mj-text nowrap flex-wrap">{dayjs(entry.createdAt).format('MM/DD/YYYY [at] hh:mm a')}<span className="pa1 ph2 ml1 br3 mb1 mr2" style={{ backgroundColor: moods[entry.moodRanking].color, color: +entry.moodRanking === 5 || +entry.moodRanking === 4 ? 'black' : 'white' }}>Mood: {moods[entry.moodRanking].mood}</span></h4>
+                        
+                        
+                        
+                        <div className="promptEntryBox ma2">
                             {togglePrompt(entry)}
 
-                        </div>
-                        <div className="ma2">
                             {toggleEntry(entry)}
                         </div>
 
 
-                        <div className=" justify-end flex">
-                            {/* <button className="pa1 ph3 ma0 mh2 mb1 br3" onClick={() => editEntry()}>Edit Entry</button> */}
+                        <div className="justify-end flex">
+                            <button className="pa1 ph3 ma0 mh2 mb1 br3" onClick={() => editEntry()}>Edit Entry</button>
                             <button className="pa1 ph3 ma0 mh2 mb1 br3" onClick={() => deleteEntry()}>Delete Entry</button>
                         </div>
+
+
+
+                        
                     </div>
                 ))}
             </div>
