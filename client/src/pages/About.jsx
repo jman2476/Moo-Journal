750e879969e00dd0d760a57a98e318be2aa70ec6
Wintino5 <wgjr85@gmail.com>
2024-03-03 20:27:11 -0500

    working on mobile

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,11 +4,11 @@ import '../styles/pages/aboutPage.scss'
 // page to explain the app and the reasons why(mental health)
 function About() {
     return (
-        <>
-        <CowPatternBg/>
+        <div className="about">
+            <CowPatternBg />
             <h1>About Page</h1>
             <p>Welcome to our app, a safe haven for your thoughts and emotions. In a world full of noise, take a moment to find your voice here. Let your feelings flow freely onto the pages of your digital journal. Whether it's joy, anger, sadness, or confusion, each entry is a step towards understanding and healing.<br />We understand the weight that burdens the mind. That's why we're here to offer a listening ear, a virtual shoulder to lean on. Our resources are here not just to guide, but to accompany you on your journey towards mental well-being. You're not alone in this struggle, and together, we can navigate the twist and turns of life's challenges.<br /> Take the first step towards a lighter heart and a clearer mind.<br /> START YOUR JOURNEY WITH US TODAY.</p>
-        </>
+        </div>
     )
 }
 
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -58,7 +58,7 @@ function Homepage() {
                         <h1 className="modak-regular"><a href="/">MooJournal</a></h1>
 
                         <h3>Start your <span className=''> MooJournaling </span> journey.</h3></span>
-                        <span>
+                        <span className="btn-container">
                             <NavLink className="btn" to='/about'>What the hell is <span className='mj-text fw8'>MooJournal</span>?</NavLink>
                             <NavLink className="btn" to='/auth'>Login | Sign Up</NavLink>
                         </span>
