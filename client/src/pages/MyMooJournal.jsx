db3157f2d5573aa411b8c69eb1c15b5089100846
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-03-04 09:05:34 -0800

    show prompt/show entry logic fixed

--- client/src/pages/MyMooJournal.jsx
+++ client/src/pages/MyMooJournal.jsx
@@ -9,11 +9,15 @@ import { useState, useEffect } from "react"
 
 function MyMooJournal() {
     console.log(moods)
-    
+
     const [showPrompt, setShowPrompt] = useState(true);
     const [showEntry, setShowEntry] = useState(true);
+    const { data: entryData, refetch } = useQuery(GET_USER_NOTES)
 
 
+    useEffect(() => {
+        refetch()
+    }, [refetch])
 
     const getDate = (timestamp) => {
         const date = new Date(timestamp);
@@ -22,49 +26,55 @@ function MyMooJournal() {
 
 
     const togglePrompt = (entry) => {
-        console.log(showPrompt)
-        return showPrompt ? (
+        return (
             <span>
-            <p className="truncate ma0 pa0">{entry.prompt.text}</p>
-            <p className="f7 ma0 pa0 pointer hover-white" onClick={() => setShowPrompt(false)}>See Full Prompt</p>  
-        </span>
-        ) : (
-            <span>
-            <p className="ma0 pa0">{entry.prompt.text}</p>
-            <p className="f7 ma0 pa0 pointer hover-white" onClick={() => setShowPrompt(true)}>Hide Prompt</p>  
-        </span>
+                <p className={showPrompt[entry._id] ? "ma0 pa0" : "truncate ma0 pa0"}>{entry.prompt.text}</p>
+                <p className="f7 ma0 pa0 pointer hover-white" onClick={() => togglePromptVis(entry._id)}>{showPrompt[entry._id] ? "Hide Prompt" : "Show Prompt"}</p>
+            </span>
         )
     }
 
     const toggleEntry = (entry) => {
-        return showEntry ? (
-            <span>
-            <p className="truncate ma0 pa0">{entry.text}</p>
-            <p className="f7 ma0 pa0 pointer hover-white" onClick={() => setShowEntry(false)}>See Full Entry</p>  
-        </span>
-        ) : (
-            <span>
-            <p className="ma0 pa0">{entry.text}</p>
-            <p className="f7 ma0 pa0 pointer hover-white" onClick={() => setShowEntry(true)}>Hide Entry</p>  
-        </span>
+        return (
+            <>
+                <p className={showEntry[entry._id] ? "ma0 pa0" : "truncate ma0 pa0" }>{entry.text}</p>
+                <p className="f7 ma0 pa0 pointer hover-white" onClick={() => toggleEntryVis(entry._id)}>{showEntry[entry._id] ? "Hide Entry" : "Show Full Entry" }</p>
+            </>
         )
     }
+
+
+    const togglePromptVis = (id) => {
+        setShowPrompt((prevShowPrompt) => ({
+            ...prevShowPrompt,
+            [id]: !prevShowPrompt[id]
+        }))
+    }
+
+    const toggleEntryVis = (id) => {
+        setShowEntry((prevShowEntry => ({
+            ...prevShowEntry,
+            [id]: !prevShowEntry[id]
+        })))
+    }
+
+
 
     const editEntry = () => {
 
     }
 
     const deleteEntry = () => {
-        
+
     }
 
-    const { data: entryData } = useQuery(GET_USER_NOTES)
+
 
     return (
 
         <>
             {/* <h1>MyMooJournal</h1> */}
-            <button onClick={() => console.log(entryData)}>Get notes</button>
+            {/* <button onClick={() => console.log(entryData)}>Get notes</button> */}
             <div className="entry-container overflow-auto flex flex-column items-start ">
                 {!entryData?.getUserEntries.length && <h2>You have not created any Entries.</h2>}
                 {entryData?.getUserEntries.map((entry, index) => (
@@ -73,18 +83,20 @@ function MyMooJournal() {
                         borderStyle: 'solid', // Ensure the border is visible by setting a style
                         borderWidth: '3px' // Set a default border width
                     }} key={entry._id} className="ba br4 ma2 journalEntryCard tl">
-                        <h4 className="w-100 flex justify-between">{dayjs(entry.createdAt).format('MM/DD/YYYY [at] hh:mm a')}<span className="pa1 ph2 ml1 br3 mb1 mr2" style={{ backgroundColor: moods[entry.moodRanking].color, color:+entry.moodRanking === 5 || +entry.moodRanking === 4  ? 'black' : 'white' }}>Mood: {moods[entry.moodRanking].mood}</span></h4>
-                        <span className="ma2">
-                        {togglePrompt(entry)}
+                        <h4 className="w-100 flex justify-between ma2 mt3 mj-text">{dayjs(entry.createdAt).format('MM/DD/YYYY [at] hh:mm a')}<span className="pa1 ph2 ml1 br3 mb1 mr2" style={{ backgroundColor: moods[entry.moodRanking].color, color: +entry.moodRanking === 5 || +entry.moodRanking === 4 ? 'black' : 'white' }}>Mood: {moods[entry.moodRanking].mood}</span></h4>
+                        <div className="ma2">
+                            {togglePrompt(entry)}
 
-                        </span>
+                        </div>
+                        <div className="ma2">
+                            {toggleEntry(entry)}
+                        </div>
 
-                        {toggleEntry(entry)}
 
-                        <span >
-                            <button onClick={() => editEntry()}>Edit</button>
-                            <button onClick={() => deleteEntry()}>Delete</button>
-                        </span>
+                        <div className=" justify-end flex">
+                            {/* <button className="pa1 ph3 ma0 mh2 mb1 br3" onClick={() => editEntry()}>Edit Entry</button> */}
+                            <button className="pa1 ph3 ma0 mh2 mb1 br3" onClick={() => deleteEntry()}>Delete Entry</button>
+                        </div>
                     </div>
                 ))}
             </div>
