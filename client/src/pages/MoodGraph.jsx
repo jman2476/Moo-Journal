550ee4124bac1756a5cba26571861dc91574b2e4
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-03-05 14:02:43 -0500

    wip

--- client/src/pages/MoodGraph.jsx
+++ client/src/pages/MoodGraph.jsx
@@ -17,17 +17,17 @@ function MoodGraph() {
         if (data) {
             let dataOrganizer = []
             let histOrganizer = [
-                {mood: 0, amnt: 0},
-                {mood: 1, amnt: 0},
-                {mood: 2, amnt: 0},
-                {mood: 3, amnt: 0},
-                {mood: 4, amnt: 0},
-                {mood: 5, amnt: 0},
-                {mood: 6, amnt: 0},
-                {mood: 7, amnt: 0},
-                {mood: 8, amnt: 0},
-                {mood: 9, amnt: 0},
-                {mood: 10, amnt: 0}
+                { mood: 0, amnt: 0 },
+                { mood: 1, amnt: 0 },
+                { mood: 2, amnt: 0 },
+                { mood: 3, amnt: 0 },
+                { mood: 4, amnt: 0 },
+                { mood: 5, amnt: 0 },
+                { mood: 6, amnt: 0 },
+                { mood: 7, amnt: 0 },
+                { mood: 8, amnt: 0 },
+                { mood: 9, amnt: 0 },
+                { mood: 10, amnt: 0 }
             ]
             console.log(data.graphMood.date)
             console.log(data.graphMood.moodRanking)
@@ -51,33 +51,41 @@ function MoodGraph() {
 
     return (
         <div className="graph-mood">
-            {data && <LineChart width={600} height={300} data={userData}>
-                <XAxis dataKey="date" />
-                <YAxis />
-                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="moodRanking" stroke="#8884d8" />
-            </LineChart>}
-            {data && 
-            <BarChart
-                width={500}
-                height={300}
-                data={histogram}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="mood" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="amnt" fill="#8884d8" activeBar={<Rectangle fill="brown" stroke="brown" />} />
-            </BarChart>
-            }
+            <div>
+
+                <h2>Your Mood over time</h2>
+                {data && <LineChart width={600} height={300} data={userData}>
+                    <XAxis dataKey="date" />
+                    <YAxis />
+                    <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="moodRanking" stroke="#8884d8" />
+                </LineChart>}
+            </div>
+            <div>
+                <h2>Mood Histogram</h2>
+                {data &&
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={histogram}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="mood" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="amnt" fill="#8884d8" activeBar={<Rectangle fill="brown" stroke="brown" />} />
+                    </BarChart>
+                }
+
+            </div>
         </div>
     )
 }
