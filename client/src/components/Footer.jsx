73b66fcb0777ba506329d4d96bbaa9cd05730ab9
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-03-04 18:28:47 -0800

    commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,9 @@
 // import grass from '../assets/Green-grass-clipart-free-pictures.png'
 import github from '../assets/Github-icon.png'
 import linkedIn from '../assets/LI-icon.png'
+import grass from '../assets/Green-grass-clipart-free-pictures.png'
+import grassMobile from '../assets/Green-grass-clipart-free-pictures-mobile.png'
+import '../styles/components/footer.scss'
 
 function Footer() {
     const contributors = [
@@ -12,49 +15,73 @@ function Footer() {
 
     ];
 
-    // const backgroundStyles = {
-    //     position: 'absolute',
-    //     top: 0,
-    //     left: 0,
-    //     right: 0,
-    //     bottom: 0,
-    //     backgroundImage: `url(${grass})`,
-    //     backgroundSize: 'cover',
-    //     backgroundRepeat: 'no-repeat',
-    //     backgroundPosition: 'center',
-    //     filter: 'blur(3px)',
-    //     zIndex: '-1',
-    // };
+    const backgroundStyles = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        // backgroundImage: `url(${grass})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        filter: 'blur(3px)',
+        zIndex: '-1',
+    };
 
-    // contentStyles = {
-    //     display: 'flex',
-    //     justifyContent: 'space-between',
-    //     alignItems: 'flex-end',
-    //     flexDirection: 'row',
-    //     marginTop: '35px',
-    // }
-    // const contributorStyles = {
-    //     listStyleType: 'none',
-    //     lineHeight: 1,
-    //     fontSize: '12px',
-    //     color: 'black',
-    //     display: 'flex',
-    //     // alignItems: 'flex-end',
-    //     marginLeft: '25px',
-    //     // marginTop: '110px'
-    //     flexDirection: 'column'
-    // }
-    // const iconStyles = {
-    //     width: '15px',
-    //     height: '15px',
-    //     marginLeft: '5px',
-    // }
+    const contentStyles = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        flexDirection: 'row',
+        marginTop: '35px',
+    }
+    const contributorStyles = {
+        listStyleType: 'none',
+        lineHeight: 1,
+        fontSize: '12px',
+        color: 'black',
+        display: 'flex',
+        // alignItems: 'flex-end',
+        marginLeft: '25px',
+        // marginTop: '110px'
+        flexDirection: 'column'
+    }
+    const iconStyles = {
+        width: '15px',
+        height: '15px',
+        marginLeft: '5px',
+    }
 
 
     return (
-        <footer style={{ position: 'absolute' }}>
-            <div className="backgroundStyles"></div>
-            <div className="contentStyles">
+        <footer>
+            {/* <div className="backgroundStyles"></div> */}
+            <div className="footer-overlay"></div>
+            <div className="imgWrap">
+                <img className="desktop" src={grass} alt="pic of grass"></img>
+                <img className="mobile" src={grassMobile} />
+            </div>
+            <div className="w-100 contributors flex flex-row pl3 justify-between">
+            <div className="flex">
+            {contributors.map((contributor, index) => ( 
+                            <span className='flex pr3' key={index}>
+                                <p className="ph0">{contributor.name}</p>
+                                <a className='icons' href={contributor.github} target="_blank" rel="noopener noreferrer">
+                                    <img src={github} alt="GitHub Icon" className="iconStyles" />
+                                </a>
+                                <a className='icons' href={contributor.linkedIn} target="_blank" rel="noopener noreferrer">
+                                    <img src={linkedIn} alt="LinkedIn Icon" className="iconStyles" />
+                                </a>
+                            </span>
+                    ))}
+                    </div>
+                    <div className='feedback pr4'>
+                        <a className="black pr2" href="about">About</a><span></span>
+                        <a className="black" href="feedback">Feedback</a>
+                    </div>
+            </div>
+            {/* <div className="contentStyles">
                 <div className="contributeList">
                     <div className='feedback pr4'>
                         <a className="btn" href="about">About</a><span></span>
@@ -74,7 +101,7 @@ function Footer() {
                         </div>
                     ))}
                 </div>
-            </div>
+            </div> */}
         </footer>
     )
 }
